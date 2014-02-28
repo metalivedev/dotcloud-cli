5d93252423a190d2959a9460ee30757f7cb61cd8
Andy Rothfusz <github@developersupport.net>
2014-02-28 14:28:16 -0800

    Added debugging statements from HTTPS libs. TODO: add hooks to turn it off.

--- dotcloud/ui/cli.py
+++ dotcloud/ui/cli.py
@@ -27,9 +27,21 @@ import tempfile
 import stat
 import platform
 import locale
+import logging
+import httplib
 
 # Set locale
 locale.setlocale(locale.LC_ALL, '')
+
+httplib.HTTPConnection.debuglevel = 1
+
+# From http://stackoverflow.com/a/16630836/1452016
+# You must initialize logging, otherwise you'll not see debug output.
+logging.basicConfig() 
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 class CLI(object):
     __version__ = VERSION
